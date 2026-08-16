1cc5ed6f217ac961e76a4f9d96956a7fca84f78e
JNCressey <42148702+JNCressey@users.noreply.github.com>
2026-08-16 23:27:21 +0100

    moved hotkey section up

--- src/main/java/com/itemrespawntimer/ItemRespawnTimerConfig.java
+++ src/main/java/com/itemrespawntimer/ItemRespawnTimerConfig.java
@@ -25,87 +25,11 @@ public interface ItemRespawnTimerConfig extends Config
 	}
 
 
-	//region removeTimersSection
-	@ConfigSection(
-			name = "Automatically Remove Timers",
-			description = "Automatically remove the expired timers",
-			position = 1,
-			closedByDefault = true
-	)
-	String removeTimersSection = "removeTimersSection";
-
-	@ConfigItem(
-			keyName = "removeTimersEvents",
-			name = "when",
-			section = removeTimersSection,
-			description = "Automatically remove timers at any of the selected conditions.",
-			position = 0
-	)
-	default Set<RemoveExpiredTimerEvent> removeTimersEvents()
-	{
-		return EnumSet.of(
-				RemoveExpiredTimerEvent.CAN_SEE_LOCATION,
-				RemoveExpiredTimerEvent.TWICE_RESPAWN_TIME
-		);
-	}
-
-	@Range(
-			max=300
-	)
-	@ConfigItem(
-			keyName = "removeTimersX",
-			name = "X seconds (for above)",
-			section = removeTimersSection,
-			description = "The time 'X' in seconds for the above 'remove when at T+X' option.",
-			position = 1
-	)
-	default int removeTimersX()
-	{
-		return 5;
-	}
-	//endregion removeTimersSection
-
-
-	//region trackedSpawnsSection
-	@ConfigSection(
-			name = "Tracked Spawns",
-			description = "Parameters of locations of spawns to track",
-			position = 1,
-			closedByDefault = true
-	)
-	String trackedSpawnsSection = "trackedSpawnsSection";
-
-	@ConfigItem(
-			keyName = "discoveryModeEnabled",
-			name = "Enable Discovery Mode",
-			section = trackedSpawnsSection,
-			description = "Will attempt to automatically update the list of tracked spawns while you observe spawns.",
-			position = 0
-	)
-	default boolean discoveryModeEnabled()
-	{
-		return true;
-	}
-
-	@ConfigItem(
-			keyName = "trackedSpawns",
-			name = "Tracked Item Spawns",
-			section = trackedSpawnsSection,
-			description = "List of data about known item spawns that this plugin will show timers for.",
-			position = 1
-	)
-	default String trackedSpawns()
-	{
-		return TrackedSpawnsDefaultFileReader.readResource();
-	}
-	//endregion trackedSpawnsSection
-
-
 	//region hotkeysSection
 	@ConfigSection(
 			name = "Hotkeys",
 			description = "Configure Hotkeys.",
-			position = 2,
+			position = 1,
 			closedByDefault = true
 	)
 	String hotkeysSection = "hotkeysSection";
@@ -146,4 +70,80 @@ public interface ItemRespawnTimerConfig extends Config
 		return RemoveExpiredTimerHotkeyMode.SINGLE;
 	}
 	//endregion hotkeysSection
+
+
+	//region removeTimersSection
+	@ConfigSection(
+			name = "Automatically Remove Timers",
+			description = "Automatically remove the expired timers",
+			position = 2,
+			closedByDefault = true
+	)
+	String removeTimersSection = "removeTimersSection";
+
+	@ConfigItem(
+			keyName = "removeTimersEvents",
+			name = "when",
+			section = removeTimersSection,
+			description = "Automatically remove timers at any of the selected conditions.",
+			position = 0
+	)
+	default Set<RemoveExpiredTimerEvent> removeTimersEvents()
+	{
+		return EnumSet.of(
+				RemoveExpiredTimerEvent.CAN_SEE_LOCATION,
+				RemoveExpiredTimerEvent.TWICE_RESPAWN_TIME
+		);
+	}
+
+	@Range(
+			max=300
+	)
+	@ConfigItem(
+			keyName = "removeTimersX",
+			name = "X seconds (for above)",
+			section = removeTimersSection,
+			description = "The time 'X' in seconds for the above 'remove when at T+X' option.",
+			position = 1
+	)
+	default int removeTimersX()
+	{
+		return 5;
+	}
+	//endregion removeTimersSection
+
+
+	//region trackedSpawnsSection
+	@ConfigSection(
+			name = "Tracked Spawns",
+			description = "Parameters of locations of spawns to track",
+			position = 3,
+			closedByDefault = true
+	)
+	String trackedSpawnsSection = "trackedSpawnsSection";
+
+	@ConfigItem(
+			keyName = "discoveryModeEnabled",
+			name = "Enable Discovery Mode",
+			section = trackedSpawnsSection,
+			description = "Will attempt to automatically update the list of tracked spawns while you observe spawns.",
+			position = 0
+	)
+	default boolean discoveryModeEnabled()
+	{
+		return true;
+	}
+
+	@ConfigItem(
+			keyName = "trackedSpawns",
+			name = "Tracked Item Spawns",
+			section = trackedSpawnsSection,
+			description = "List of data about known item spawns that this plugin will show timers for.",
+			position = 1
+	)
+	default String trackedSpawns()
+	{
+		return TrackedSpawnsDefaultFileReader.readResource();
+	}
+	//endregion trackedSpawnsSection
 }
